cb95897a0c67881e28445bc68d4806e4f5d2b57d
Hans Krupakar <hanskrupakar1994@gmail.com>
2016-09-08 08:31:15 +0530

    Update WebController.java

--- User Registration API (mySQL)/src/main/java/hans/WebController.java
+++ User Registration API (mySQL)/src/main/java/hans/WebController.java
@@ -29,7 +29,7 @@ public class WebController extends WebMvcConfigurerAdapter {
     public DriverManagerDataSource dataSource() {
 	DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 	driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/hcl");
+	driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/db");
 	driverManagerDataSource.setUsername("root");
 	driverManagerDataSource.setPassword("root");
 	this.dataSource=driverManagerDataSource;
